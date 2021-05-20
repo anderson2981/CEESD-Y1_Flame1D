91407aa4642b5b16dce223c2c894d363e3dba518
Michael Anderson <manders2@illinois.edu>
2021-05-20 09:06:03 -0700

    add hello_world

--- experiments/parsl/hello_world/run_hello_parsl.py
+++ experiments/parsl/hello_world/run_hello_parsl.py
@@ -11,10 +11,9 @@ from parsl.data_provider.files import File
 from parsl.config import Config
 from parsl.executors.threads import ThreadPoolExecutor
 
-local_threads = Config(
-    executors=[
-        ThreadPoolExecutor(max_threads=8, label='local_threads')
-    ]
+local_thread_executor = ThreadPoolExecutor(max_threads=8, label='local_threads')
+local_thread_config = Config(
+    executors=[local_thread_executor]
 )
 
 # a configuration to run locally with pilot jobs
@@ -27,100 +26,102 @@ from parsl.launchers import JsrunLauncher
 from parsl.launchers import SrunLauncher
 from parsl.addresses import address_by_interface
 
-local_htex = Config(
-    executors=[
-        HighThroughputExecutor(
-            label="htex_Local",
-            worker_debug=True,
-            cores_per_worker=1,
-            provider=LocalProvider(
-                channel=LocalChannel(),
-                init_blocks=1,
-                max_blocks=1,
-            ),
-        )
-    ],
+local_htex = HighThroughputExecutor(
+    label="htex_Local",
+    worker_debug=True,
+    cores_per_worker=1,
+    provider=LocalProvider(
+        channel=LocalChannel(),
+        init_blocks=1,
+        max_blocks=1,
+    ),
+)
+local_htex_config = Config(
+    executors=[local_htex]
+)
+
+lassen_htex =  HighThroughputExecutor(
+    label="Lassen_HTEX",
+    working_dir='/p/gpfs1/manders/',
+    #address=address_by_interface('ib0'),   # assumes Parsl is running on a login node
+    address='lassen.llnl.gov',   # assumes Parsl is running on a login node
+    worker_port_range=(50000, 55000),
+    worker_debug=True,
+    provider=LSFProvider(
+        launcher=JsrunLauncher(overrides='-g 1 -a 1'),
+        walltime="00:20:00",
+        nodes_per_block=1,
+        init_blocks=1,
+        max_blocks=1,
+        worker_init=(
+            'export XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"\n'
+            'export PYOPENCL_CTX="port:tesla"\n'
+        ),
+        project='uiuc',
+        cmd_timeout=600
+    ),
+)
+lassen_htex_config = Config(
+    executors=[lassen_htex],
     strategy=None,
 )
 
-lassen_htex = Config(
-    executors=[
-        HighThroughputExecutor(
-            label="Lassen_HTEX",
-            working_dir='/p/gpfs1/manders/',
-            #address=address_by_interface('ib0'),   # assumes Parsl is running on a login node
-            address='lassen.llnl.gov',   # assumes Parsl is running on a login node
-            worker_port_range=(50000, 55000),
-            worker_debug=True,
-            provider=LSFProvider(
-                launcher=JsrunLauncher(overrides='-g 1 -a 1'),
-                walltime="00:20:00",
-                nodes_per_block=1,
-                init_blocks=1,
-                max_blocks=1,
-                worker_init=(
-                             'export XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"\n'
-                             'export PYOPENCL_CTX="port:tesla"\n'
-                            ),
-                project='uiuc',
-                cmd_timeout=600
-            ),
-        )
-    ],
-    strategy=None,
+quartz_htex = HighThroughputExecutor(
+    label="Quartz_HTEX",
+    working_dir='/p/lscratchh/manders/work/CEESD/parsl_remote/flame1d',
+    address='quartz.llnl.gov',   # assumes Parsl is running on a login node
+    #worker_port_range=(50000, 55000),
+    worker_debug=True,
+    cores_per_worker=2,
+    provider=SlurmProvider(
+        launcher=SrunLauncher(),
+        walltime="00:20:00",
+        nodes_per_block=1,
+        init_blocks=1,
+        max_blocks=1,
+        worker_init=(
+            'export XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"\n'
+            'export OMPI_MCA_mpi_warn_on_fork=0\n'
+            #'export PYOPENCL_CTX="port:pthread"\n'
+            'module load gcc/10.2.1\n'
+            'module load openmpi/3.0.1\n'
+        ),
+        account='uiuc',
+        #partition='pbatch',
+        partition='pdebug',
+        cmd_timeout=600
+    ),
+)
+quartz_htex_config = Config(
+    executors=[quartz_htex],
+    strategy=None
 )
 
-quartz_htex = Config(
-    executors=[
-        HighThroughputExecutor(
-            label="Quartz_HTEX",
-            working_dir='/p/lscratchh/manders/work/CEESD/parsl_remote/flame1d',
-            address='quartz.llnl.gov',   # assumes Parsl is running on a login node
-            worker_port_range=(50000, 55000),
-            worker_debug=True,
-            cores_per_worker=2,
-            provider=SlurmProvider(
-                launcher=SrunLauncher(),
-                walltime="00:20:00",
-                nodes_per_block=1,
-                init_blocks=1,
-                max_blocks=1,
-                worker_init=(
-                             'export XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"\n'
-                             'export OMPI_MCA_mpi_warn_on_fork=0\n'
-                             #'export PYOPENCL_CTX="port:pthread"\n'
-                             'module load gcc/10.2.1\n'
-                             'module load openmpi/4.1.0\n'
-                            ),
-                account='uiuc',
-                #partition='pbatch',
-                partition='pdebug',
-                cmd_timeout=600
-            ),
-        )
-    ],
-    strategy=None,
+all_sites_config = Config(
+    executors=[local_htex, quartz_htex, lassen_htex],
+    strategy=None
 )
 
 print(parsl.__version__)
 parsl.set_stream_logger()
 
 parsl.clear()
-#parsl.load(local_threads)
-#parsl.load(local_htex)
-#parsl.load(lassen_htex)
-parsl.load(quartz_htex)
+#parsl.load(local_threads_config)
+#parsl.load(local_htex_config)
+#parsl.load(lassen_htex_config)
+parsl.load(quartz_htex_config)
+#parsl.load(all_sites_config)
 
 # build a string that loads conda correctly
 def load_conda():
    return(
             'module load gcc/10.2.1\n'
-            #'module load openmpi/4.1.0\n'
-            'CONDA_BASE=$(conda info --base)\n'
-            'source ${CONDA_BASE}/etc/profile.d/conda.sh\n'
-            'conda deactivate\n'
-            'conda activate mirgeDriver.flame1d\n'
-            'which python\n'
+            'module load openmpi/3.0.1\n'
+            #'CONDA_BASE=$(conda info --base)\n'
+            #'source ${CONDA_BASE}/etc/profile.d/conda.sh\n'
+            #'conda deactivate\n'
+            #'conda activate mirgeDriver.flame1d\n'
+            #'which python\n'
             'which mpicc\n'
             'module list\n'
             'env\n'
@@ -154,7 +155,6 @@ print('Done: {}'.format(flame_init.done()))
 print('Result: {}'.format(flame_init.result()))
 print('Done: {}'.format(flame_init.done()))
 
-# second run is a restart from the init, on lassen
 # second run with python
 intro_str = 'echo "python hello-world-mpi"\n'
 conda_str = load_conda()
