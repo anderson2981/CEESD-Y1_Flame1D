46c4a58f9e3e2efe9889f482d48bff2559fb5a6a
Michael Anderson <manders2@illinois.edu>
2021-07-20 12:46:18 -0700

    updates

--- flame1d.py
+++ flame1d.py
@@ -570,6 +570,8 @@ def main(ctx_factory=cl.create_some_context, casename="flame1d", user_input_file
         if errors:
             raise RuntimeError("Error detected by user checkpoint, exiting.")
 
+        return dt
+
     if rank == 0:
         logging.info("Stepping.")
 
